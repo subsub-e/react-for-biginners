d6baa5efeb595285f50c853cffe7b0eef839731c
subsub-e <ab13696802@gmail.com>
2024-06-30 17:20:35 +0900

    style 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Coin from "./components/Coin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Coin />
   </React.StrictMode>
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,40 +1,44 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-
   return (
-    <div>
-      <h1>영화 선택 사이트</h1>
+    <div className={styles.container}>
       {loading ? (
-        <strong>Loading...</strong>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        movies.map((movie) => (
-          <Movie
-            key={movie.id}
-            id={movie.id}
-            medium_cover_image={movie.medium_cover_image}
-            title={movie.title}
-            summary={movie.summary}
-            genres={movie.genres}
-          />
-        ))
+        <div className={styles.movies}>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
 }
-
 export default Home;
